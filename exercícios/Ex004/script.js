6635fce91f466b7f044ce6bee4ec09a3c16a66a9
leticiakaren2015 <leticiakaren2015@gmail.com>
2026-03-05 14:55:34 +0000

    Usando Function

---
+++ exercícios/Ex004/script.js
@@ -0,0 +1,10 @@
+function calcular () {
+    var txtv = document.getElementById('txtvel')
+    var res = document.getElementById('res')
+    var vel =Number(txtv.value)
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}km/h</strong></p>`
+    if (vel > 60){
+        res.innerHTML += `<p>Você está <strong>MULTADO</strong> por excesso de velocidade!</p>`
+    }
+    res.innerHTML += `<p>Dirija sempre com o cinto de segurança!</p>`
+}
