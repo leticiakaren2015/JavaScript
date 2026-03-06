a9422cf50c62a62363900182e9fd0a60e00266aa
leticiakaren2015 <leticiakaren2015@gmail.com>
2026-03-06 10:30:01 +0000

    mostrado a hora atual

--- exercícios/Ex005/script.js
+++ exercícios/Ex005/script.js
@@ -2,23 +2,19 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22 // forçar a hora para teste
+    var hora = data.getHours()
+    //var hora = 22 // forçar a hora para teste
 
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12) {
         img.src = 'imagens/manha.jpg'
-        msg.innerHTML = 'Bom dia!'
         document.body.style.background = '#ffe26c'
     } else if (hora >= 12 && hora <= 18) {
         img.src = 'imagens/tarde.jpg'
-        msg.innerHTML = 'Boa tarde!'
         document.body.style.background = '#f2a762'
-        
     } else {
         img.src = 'imagens/noite.jpg'
-        msg.innerHTML = 'Boa noite!'
         document.body.style.background = '#002436'
     }
 }
